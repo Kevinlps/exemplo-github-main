684ed6109dca987ae81fbe386bbf574e0252b26e
Kevin <Kevin@email.com>
2022-03-15 19:46:36 -0400

    atualizado

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const $ = document.querySelector.bind(document)
+
+const addTask = (event) => {
+  event.preventDefault()
+  const taskDescription = $('#task-description').value
+
+  const newTaskItem = `
+  <li>${taskDescription}</li>
+  `
+  $('#task-list').insertAdjacentHTML('beforeend', newTaskItem)
+  $('#task-form').reset()
+}
